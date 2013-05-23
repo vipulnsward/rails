6ab458f474408767df21934393416b4b0891a294
Vipul A M <vipulnsward@gmail.com>
2013-05-23 18:56:26 +0530

    Handle and scrub ending _controller gracefully in controller generator

--- railties/lib/rails/generators/rails/controller/controller_generator.rb
+++ railties/lib/rails/generators/rails/controller/controller_generator.rb
@@ -5,13 +5,21 @@ module Rails
       check_class_collision suffix: "Controller"
 
       def create_controller_files
-        template 'controller.rb', File.join('app/controllers', class_path, "#{file_name}_controller.rb")
+        template 'controller.rb', File.join('app/controllers', class_path, "#{scrub_file_name}_controller.rb")
       end
 
       def add_routes
         actions.reverse.each do |action|
           route %{get "#{file_name}/#{action}"}
         end
+      end
+
+      def class_name
+        (class_path + [scrub_file_name]).map!{ |m| m.camelize }.join('::')
+      end
+
+      def scrub_file_name
+        @scrubbed_name ||= file_name.gsub(/_controller$/,'')
       end
 
       hook_for :template_engine, :test_framework, :helper, :assets
